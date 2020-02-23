627b9b84195a73acabd575afea2ed15b967dd80e
Nithin M Warrier <nithin.warier@gmail.com>
2020-02-23 12:49:02 +0530

    Update User.kt
    make data class for user

--- ShowList/app/src/main/java/com/androidians/showlist/User.kt
+++ ShowList/app/src/main/java/com/androidians/showlist/User.kt
@@ -1,15 +1,3 @@
 package com.androidians.showlist
 
-class User {
-    var name: String = ""
-    var email: String = ""
-    var age: Int = 0
-
-    constructor()
-
-    constructor(name: String, email: String, age: Int) {
-        this.name = name
-        this.email = email
-        this.age = age
-    }
-}
+data class User(private val name: String, private val email: String, privat val age: Int)
